c1ca89c87ffd066618a286e91ed0420af63de653
ikramkharbouch <4573r14@gmail.com>
2021-05-31 19:51:43 +0100

    Add hygiene page

---
+++ components/Card.js
@@ -0,0 +1,17 @@
+const Card = ({ title, image, text, id }) => {
+  return (
+    <>
+      <div className="card shadow-lg rounded-md" key={id}>
+      <img src={image} width="100px" height="100px" className="mx-auto mt-5"/>
+      <div className="mt-2">
+          <h1 className="text-xs lg:text-sm font-bold mx-4">{title}</h1>
+          <p className="text-xs lg:text-sm mt-2 mx-4">{text}</p>
+      </div>
+  </div>
+    </>
+  );
+};
+
+export default Card;
+
+// old stuff
---
+++ pages/hygiene.js
@@ -0,0 +1,61 @@
+import Card from '../components/Card'
+
+const hygiene = () => {
+
+const info = [
+	{
+        id: '1',
+		title: 'La chaîne du froid      entre 0 et 3°Celsius',
+		text:
+			'doit être contrôlée en permanance et étalonnée à fréquence régulière.',
+		image: "images/cold.jpg",
+	},
+	{
+        id: '2',
+		title: 'La chaîne du chaud',
+		text:
+			'un aliment doit être rapidement monté en température supérieure à 63°Celsius et y être maintenu.',
+		image: "images/hot.jpg",
+	},
+	{
+        id: '3',
+		title: 'Emballage adaptés aux denrées alimentaires',
+		text:
+			'Ils doivent être faciles à nettoyer et à désinfecter en répondant aux exigences réglementaires.',
+		image: "images/foodpack.jpg",
+	},
+	{
+        id: '4',
+		title: 'Alimentation en eau',
+		text:
+			"L'alimentation en eau potable doit être disponible, pour éviter la contamination des denrées alimentaires et un examen bactériologique doit être effectuée avec une fréquence trimestrielle.",
+		image: "images/water.jpg",
+	},
+	{
+        id: '5',
+		title: 'Hygiène personnelle',
+		text:
+			'Il est exigé que toute personne travaillant dans une zone de manutention de denrées alimentaires respecte un niveau élevé de propreté personnelle et porte des tenues adaptées (gants, masques, coiffes, tabliers).',
+		image: "images/staff.jpg",
+	},
+	{
+        id: '6',
+		title: 'Les locaux',
+		text:
+			"Répondent au principe de la marche en avant, les revêtements de sol, surfaces de manipulation, les murs et les portes doivent être bien entretenus, faciles à nettoyer et, au besoin désinfectés. L'utilisation de matériaux étanches, lisses, non absorbants, lavables et non toxiques est nécessaire.",
+		image: "images/local.jpg",
+	},
+]
+
+    return ( 
+        <>
+            <div className="flex flex-wrap mx-auto flex-col lg:flex-row w-full">
+            {info.map((card) => (
+					<Card image={card.image} title={card.title} text={card.text} id={card.id} key={card.id}/>
+				))}
+            </div>
+        </>
+     );
+}
+ 
+export default hygiene;
