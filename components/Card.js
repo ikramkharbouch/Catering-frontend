091d0dffa5f819f261a58055e1cc86f0ed728321
ikramkharbouch <4573r14@gmail.com>
2021-05-31 19:56:27 +0100

    update: update styling

--- components/Card.js
+++ components/Card.js
@@ -4,8 +4,8 @@ const Card = ({ title, image, text, id }) => {
       <div className="card shadow-lg rounded-md" key={id}>
       <img src={image} width="100px" height="100px" className="mx-auto mt-5"/>
       <div className="mt-2">
-          <h1 className="text-xs lg:text-sm font-bold mx-4">{title}</h1>
-          <p className="text-xs lg:text-sm mt-2 mx-4">{text}</p>
+          <h1 className="text-sm font-bold mx-4">{title}</h1>
+          <p className="text-sm mt-2 mx-4">{text}</p>
       </div>
   </div>
     </>
