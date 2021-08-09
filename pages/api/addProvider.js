755da42025802f5cdc17725162bf2198302a63d5
ikramkharbouch <4573r14@gmail.com>
2021-08-09 23:57:09 +0100

    api: add api route for registering providers

---
+++ pages/api/addProvider.js
@@ -0,0 +1,23 @@
+export default async function addProvider(req, res) {
+
+    if (req.method === 'POST') {
+        const result = await fetch(process.env.url + "/v1/providers", {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(req.body)
+          });
+          const status = result.status;
+          if (status == 200)
+          {
+            res.status(200);
+            res.end()
+          } else {
+            res.status(500);
+            res.end();
+          }
+    } else {
+      res.status(430).json({ status: "ok" })
+    }
+  }
