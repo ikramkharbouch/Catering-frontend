4e4998358674da7eb0f666929cffeaef909bff89
ikramkharbouch <4573r14@gmail.com>
2021-11-04 17:30:13 +0100

    Update auth

---
+++ pages/Auth/withAuth.js
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+
+const WithAuth = (WrappedComponent) => {
+  return (props) => {
+    if (typeof window !== "undefined") {
+      const router = useRouter();
+
+      if (typeof localStorage !== "undefined") {
+        var loggedIn = localStorage.getItem("loggedIn");
+      } else {
+        var loggedIn = localStorage.setItem("loggedIn", false);
+      }
+
+      console.log(loggedIn);
+
+      if (!loggedIn) {
+        router.replace("/error");
+        return null;
+      }
+      return <WrappedComponent {...props} />;
+    }
+
+    // Just in case we are on the server
+    return null;
+  };
+};
+
+export default WithAuth;
